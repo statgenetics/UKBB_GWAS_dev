1453a0d02dbb827f78c308dc8f96a10a0ae11f76
Gao Wang <wangow@gmail.com>
2020-05-15 13:50:37 -0400

    Update HTML

---
+++ docs/js/docs.js
@@ -0,0 +1,6 @@
+var workflowArray = ['BoltLMM', 'SAIGE', 'fastGWA', 'postprocessing', '101_Manhattan_plot', 'setup']
+var workflowDict = {'BOLT-LMM-analyses-for-UK-Biobank-data-1': 'BoltLMM', 'SAIGE-analysis-for-UK-Biobank-data-1': 'SAIGE', 'fastGWA-analyses-for-UK-Biobank-data-1': 'fastGWA', 'Data-analysis-1': 'postprocessing', 'Setting-up-the-computer-environment-1': 'setup'}
+var workflowArrayMap = {'BoltLMM': 'BOLT-LMM analyses for UK Biobank data', 'SAIGE': 'SAIGE analysis for UK Biobank data', 'fastGWA': 'fastGWA analyses for UK Biobank data', 'postprocessing': 'Data analysis', 'setup': 'Setting up the computer environment'}
+var analysisArray = ['phenotype_analysis']
+var analysisDict = {'Phenotype-data-summary-and-transformation-1': 'phenotype_analysis'}
+var analysisArrayMap = {'phenotype_analysis': 'Phenotype data summary  ... ansformation'}
