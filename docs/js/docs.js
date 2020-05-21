ab3ac0641fa87d0ad0ac4ad54246d91b3307defd
Gao Wang <wangow@gmail.com>
2020-05-21 11:43:42 -0400

    Rename README

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,4 +1,4 @@
-var workflowArray = ['BoltLMM', 'SAIGE', 'fastGWA', 'postprocessing', '101_Manhattan_plot', 'setup']
+var workflowArray = ['BoltLMM', 'SAIGE', 'fastGWA', 'postprocessing', 'setup']
 var workflowDict = {'BOLT-LMM-analyses-for-UK-Biobank-data-1': 'BoltLMM', 'SAIGE-analysis-for-UK-Biobank-data-1': 'SAIGE', 'fastGWA-analyses-for-UK-Biobank-data-1': 'fastGWA', 'Data-analysis-1': 'postprocessing', 'Setting-up-the-computer-environment-1': 'setup'}
 var workflowArrayMap = {'BoltLMM': 'BOLT-LMM analyses for UK Biobank data', 'SAIGE': 'SAIGE analysis for UK Biobank data', 'fastGWA': 'fastGWA analyses for UK Biobank data', 'postprocessing': 'Data analysis', 'setup': 'Setting up the computer environment'}
 var analysisArray = ['phenotype_analysis']
