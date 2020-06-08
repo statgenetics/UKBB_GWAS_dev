c1b8d31c99c89ce992f3ab6d96090dac8415a464
dianacornejo <dcornejo88@gmail.com>
2020-06-08 18:01:02 -0400

    fixes

--- analysis/MWE.sh
+++ analysis/MWE.sh
@@ -10,13 +10,13 @@ phenoFile=~/scratch60/plink-clumping/MWE/phenotypes.txt
 covarFile=~/scratch60/plink-clumping/MWE/phenotypes.txt
 LDscoresFile=~/software/BOLT-LMM_v2.3.4/tables/LDSCORE.1000G_EUR.tab.gz
 geneticMapFile=~/software/BOLT-LMM_v2.3.4/tables/genetic_map_hg19_withX.txt.gz
-phenoCol= 'BMI'
-covarCol1 = 'SEX'
-covarMaxLevels = 10
-qCovarCol2 = 'AGE'
-numThreads = 5
-bgenMinMAF = 0.001
-bgenMinINFO = 0.1
+phenoCol='BMI'
+covarCol1='SEX'
+covarMaxLevels=10
+qCovarCol2='AGE'
+numThreads=5
+bgenMinMAF=0.001
+bgenMinINFO=0.1
 
 #Running the workflow for the MWE
 
